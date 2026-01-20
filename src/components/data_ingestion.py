3a5715589c2d8a993a27707f6d450b9fff3e807e
Abhishek Singh <abhishek25october2005@gmail.com>
2026-01-20 17:57:35 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from src.exception import CustomException
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig 
+from src.components.model_trainer import ModelTrainerConfig , ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -52,5 +53,7 @@ if __name__ == "__main__":
     data_ingestion = DataIngestion()
     train_path, test_path = data_ingestion.initiate_data_ingestion(file_path='notebook/data/stud.csv')
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_path, test_path)
-    logging.info("Data transformation initiated.")
+    train_arr,test_arr , _ = data_transformation.initiate_data_transformation(train_path, test_path)
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+    
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import numpy as np
 import pandas as pd
+from sklearn.metrics import r2_score
 from src.exception import CustomException
 from src.logger import logging
 import pickle 
@@ -15,4 +16,19 @@ def save_object(file_path, obj):
             pickle.dump(obj, file_obj)
     except Exception as e:
         logging.error("Error in save_object: %s", e)
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        for model_name, model in models.items():
+            logging.info(f"Training model: {model_name}")
+            model.fit(X_train, y_train)
+            y_test_pred = model.predict(X_test)
+            r2_square = r2_score(y_test, y_test_pred)
+            report[model_name] = r2_square
+            logging.info(f"{model_name} R2 Score: {r2_square}")
+        return report
+    except Exception as e:
+        logging.error("Error in evaluate_models: %s", e)
         raise CustomException(e, sys)
